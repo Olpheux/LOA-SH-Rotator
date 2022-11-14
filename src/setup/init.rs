5c64718f37d5470c11efb166d65d1fe1ca185243
Alice <alicemc@collum.ca>
2022-11-14 12:47:59 -0800

    Better error handling for when file unopenable

--- src/setup/init.rs
+++ src/setup/init.rs
@@ -3,21 +3,31 @@ use crate::setup::setup_from_input;
 use crate::setup::setup_from_file;
 use crate::setup::setup_skills;
 use crate::character::char_structs::meta_structs::Character as char_struct;
-use std::path::Path;
+use std::io::ErrorKind;
 
 pub fn init() -> char_struct{
     let filename = "./config.json";
-    let character_file_exists = Path::new(filename).is_file();
     let mut character: char_struct;
     let runes: Vec<RuneLevels>;
     
-    if character_file_exists {
-        let file_opened = std::fs::read_to_string(filename); 
-        character = setup_from_file::setup_from_file((file_opened.as_ref()).unwrap()); //I don't know why this needs .as_ref() instead of & either.
-        runes = setup_from_file::get_runes::get(&(file_opened).unwrap());
-    } else {
-        character = setup_from_input::setup_from_input();
-        runes = setup_from_input::get_runes::get();
+    let file_opened = std::fs::read_to_string(filename);
+    
+    match file_opened {
+        Ok(_) => {
+            character = setup_from_file::setup_from_file((file_opened.as_ref()).unwrap()); //I don't know why this needs .as_ref() instead of & either.
+            runes = setup_from_file::get_runes::get(&(file_opened).unwrap());
+        }
+        Err(error) => { 
+            match error.kind() {
+                ErrorKind::NotFound => println!("Config file not found."),
+                ErrorKind::PermissionDenied => println!("Read permissions denied."),
+                ErrorKind::InvalidData => println!("Unable to read file properly! May contain non-UTF8 characters?"),
+                _ => println!("Unknown error! Error seen: {:?}", error.kind())
+            }
+            println!("Falling back to input-based setup...");
+            character = setup_from_input::setup_from_input();
+            runes = setup_from_input::get_runes::get();
+        }
     };
 
     character.skills = setup_skills::skill_setup(&character.stats, runes);

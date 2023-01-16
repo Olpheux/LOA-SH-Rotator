3fa4568fe8ada1a23e860a2e673043d3a92feb02
Alice <alicemc@collum.ca>
2023-01-16 15:59:25 -0800

    Bugfix

--- src/setup/init.rs
+++ src/setup/init.rs
@@ -1,21 +1,19 @@
-use crate::character::other_structs::RuneLevels;
+#![allow(clippy::needless_late_init)]
+
 use crate::setup::setup_from_input;
 use crate::setup::setup_from_file;
-use crate::setup::setup_skills;
 use crate::character::meta_structs::Character as char_struct;
 use std::io::ErrorKind;
 
 pub fn init() -> char_struct{
     let filename = "./config.json";
-    let mut character: char_struct;
-    let runes: Vec<RuneLevels>;
+    let character: char_struct;
     
     let file_opened = std::fs::read_to_string(filename);
     
     match file_opened {
         Ok(_) => {
             character = setup_from_file::setup_from_file((file_opened.as_ref()).unwrap()); //I don't know why this needs .as_ref() instead of & either.
-            runes = setup_from_file::get_runes::get(&(file_opened).unwrap());
         }
         Err(error) => { 
             match error.kind() {
@@ -26,11 +24,8 @@ pub fn init() -> char_struct{
             }
             println!("Falling back to input-based setup...");
             character = setup_from_input::setup_from_input();
-            runes = setup_from_input::get_runes::get();
         }
     };
-
-    character.skills = setup_skills::skill_setup(&character.stats, runes);
 
     character
 }
